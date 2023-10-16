3ab852061919850615c08893b1fdc5c35a2e192e
yeongbin05 <bigstar96115@hanmail.net>
2023-10-17 00:38:36 +0900

    test

--- chats/models.py
+++ chats/models.py
@@ -10,7 +10,7 @@ class Message(models.Model):
 	body = models.TextField(max_length=1000, blank=True, null=True)
 	date = models.DateTimeField(auto_now_add=True)
 	is_read = models.BooleanField(default=False)
-
+	#test
 	def send_message(from_user, to_user, body):
 		sender_message = Message(
 			user=from_user,
